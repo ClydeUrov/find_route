e1861faee926efa1e0a661028aa573d476f62fcc
ClydeUrov <clydeurov@gmail.com>
2023-01-09 15:25:01 +0200

    Оформление навигации.

--- cities/urls.py
+++ cities/urls.py
@@ -3,5 +3,6 @@ from cities.views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    
+    path('<int:pk>/', home, name='home'),
+
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from cities.models import City
 
 
@@ -6,7 +6,13 @@ __all__ = (
 	'home',
 )
 
-def home(request):
+def home(request, pk=None):
+	if pk:
+		#city = City.objects.filter(id=pk).first()
+		#city = City.objects.get(id=pk)
+		city = get_object_or_404(City, id=pk)
+		context = {'object': city}
+		return render(request, 'cities/detail.html', context)
 	qs = City.objects.all()
 	context = {'object_list': qs}
 	return render(request, 'cities/home.html', context)
