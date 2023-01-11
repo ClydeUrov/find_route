1d3761666cccfeead7c66ba1fa08a7a8a6a3a58f
ClydeUrov <clydeurov@gmail.com>
2023-01-11 14:32:28 +0200

    Добавил всплывающие окна при определённых действиях.

--- cities/views.py
+++ cities/views.py
@@ -1,8 +1,9 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
-
+from django.contrib import messages
 from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
@@ -39,18 +40,20 @@ class CityDetailView(DetailView):
 	template_name = 'cities/detail.html'
 
 
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin, CreateView):
 	model = City
 	form_class = CityForm
 	template_name = 'cities/create.html'
 	success_url = reverse_lazy('cities:home')
+	success_message = "Город успешно создан"
 
 
-class CityUpdateView(UpdateView):
+class CityUpdateView(SuccessMessageMixin, UpdateView):
 	model = City
 	form_class = CityForm
 	template_name = 'cities/update.html'
 	success_url = reverse_lazy('cities:home')
+	success_message = "Город успешно отредактирован"
 
 class CityDeleteView(DeleteView):
 	model = City
@@ -58,6 +61,7 @@ class CityDeleteView(DeleteView):
 	success_url = reverse_lazy('cities:home')
 
 	def get(self, request, *args, **kwargs):
+		messages.success(request, 'Город успешно удалён')
 		return self.post(request, *args, **kwargs) # Без подтверждения
 
 
