e49b60bade2196f10c8f21e4b6116fc7bd2f3c5e
ClydeUrov <clydeurov@gmail.com>
2023-01-12 16:06:34 +0200

    Добавил новое приложение и создал страницу с поездами.

--- travel/urls.py
+++ travel/urls.py
@@ -1,4 +1,5 @@
-"""travel URL Configuration
+"""
+    travel URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -10,7 +11,7 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import to include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
@@ -20,6 +21,7 @@ from travel.views import home, about
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
+    path('trains/', include(('trains.urls', 'trains'))),
     path('', home, name='home'),
     path('about/', about),
 ]
