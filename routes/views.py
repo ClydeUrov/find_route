b29359d7b23b360c4ecb3ad4b1e4b7b670d4bb02
ClydeUrov <clydeurov@gmail.com>
2023-01-16 11:11:00 +0200

    Добавил функцию find_routes

--- routes/views.py
+++ routes/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.contrib import messages
 from routes.forms import RouteForm
 
 def home(request):
     form = RouteForm()
     return render(request, 'routes/home.html', {'form': form})
+
+def find_routes(request):
+    if request.method == "POST":
+        form = RouteForm(request.POST)
+        a = 1
+        return render(request, 'routes/home.html', {'form': form})
+    else:
+        print(1)
+        form = RouteForm()
+        messages.error(request, 'Нет данных для поиска')
+        return render(request, 'routes/home.html', {'form': form})
