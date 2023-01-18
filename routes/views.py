3b1b5111096800e6624ab7bc557828e3cfc35fcb
ClydeUrov <clydeurov@gmail.com>
2023-01-18 16:45:45 +0200

    Оформил удаление и запрос входа в аккаунт.

--- routes/views.py
+++ routes/views.py
@@ -1,14 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.urls import reverse_lazy
 
 from cities.models import City
 from routes.forms import RouteForm, RouteModelForm
 from routes.models import Route
 from routes.utils import get_routes
 from trains.models import Train
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, DeleteView
 
 
+# @login_required
 def home(request):
     form = RouteForm()
     return render(request, 'routes/home.html', {'form': form})
@@ -81,3 +85,12 @@ class RouteListView(ListView):
 class RouteDetailView(DetailView):
 	queryset = Route.objects.all()
 	template_name = 'routes/detail.html'
+
+
+class RouteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Route
+    success_url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Маршрут успешно удалён')
+        return self.post(request, *args, **kwargs) # Без подтверждения
